f3ae03dbd6f626263596f7ef7afa5f2238e33a2f
mujid ikhram <mujidikram@gmail.com>
2024-08-02 01:34:16 +0700

    create index

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // Ini Javascipt
+
+let nameInput = document.getElementById('name-input');
+
+document.getElementById('SEND').addEventListener("click", function() {
+    const nama = nameInput.value
+
+    document.getElementById('nameInput'). innerHTML = nameInput
+})
